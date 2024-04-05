3b921ef0e147a8d92058bab0fc97c624f0cd8e7e
Ttst <thibautstachnick@gmail.com>
2024-04-05 11:53:47 +0200

    add axios

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+createApp(App).use(VueAxios, axios)
 
 createApp(App).mount('#app')
