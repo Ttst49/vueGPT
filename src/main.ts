78642039eb2aee338c9e29e94476a522dd31c521
Ttst <thibautstachnick@gmail.com>
2024-04-06 14:30:22 +0200

    add prime components and rearrange app mount

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,29 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-createApp(App).use(VueAxios, axios)
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Textarea from "primevue/textarea";
+import Avatar from "primevue/avatar";
+import Dropdown from "primevue/dropdown";
+import ProgressBar from "primevue/progressbar";
+import Dialog from "primevue/dialog";
+import InputText from "primevue/inputtext";
 
-createApp(App).mount('#app')
+import "primevue/resources/themes/aura-light-green/theme.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component("Button", Button);
+app.component("Textarea", Textarea);
+app.component("Avatar", Avatar);
+app.component("Dropdown", Dropdown);
+app.component("ProgressBar", ProgressBar);
+app.component("Dialog", Dialog);
+app.component("InputText", InputText);
+app.use(VueAxios, axios)
+app.mount('#app')
